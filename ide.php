a78d3d404ff275c87ec4f05c87fbeebef513b927
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-11-06 18:08:58 +0700

    Create ide.php

---
+++ ide.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row row-height">
+            <div class="col-md-6 cell">
+                <div class="cell-body">
+                    <h3>ITF</h3>
+                    <p1>นาย นวิน เวชสุวรรณ์ รหัส 63070232</p1>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row height-100">
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>จันทร์</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>อังคาร</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>พุธ</h3>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 66%;">
+                                <div class="cell-body">
+                                    <h3>พฤหัส</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 34%;">
+                                <div class="cell-body">
+                                    <h3>ศุกร์</h3>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
